a408adb6b89c4ad054573609189190e259c8bcb5
Fábio <126986071+FBorbaDev@users.noreply.github.com>
2024-12-15 16:21:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    if (quantidade > (ate - de + 1)) {
+        alert("A quantidade solicitada é maior do que o intervalo de números disponíveis.");
+        return;
+    }
+
+    let sorteados = [];
+    let numero;
+    
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números da Sorte: ${sorteados}`;
+    alterarStatusBotao();
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById("btn-reiniciar");
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    } else {
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+function reiniciar() {
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById("resultado").innerHTML = `<label class="texto__paragrafo">Números do Milhão!`;
+    alterarStatusBotao();
+}
